586e2ee07d95688dc19b367998b7195a30afb40e
clara <clara@clara.com>
2018-01-08 11:55:03 -0600

    Organize examples

--- javascript/helloworld.js
+++ javascript/helloworld.js
@@ -1,3 +1,5 @@
+/*** BASIC VARIABLES ***/
+
 //Integer variable
 var quantity = 6;
 
@@ -5,16 +7,38 @@ var quantity = 6;
 var price = 123.45;
 
 //String variables. Use single or double quotes
-var text = 'hello world';
+var hello = 'hello world';
 var moreText = "double quotes are ok too";
+
+// Boolean variables. true and false are keywords
+var easyMode = true;
+var summer = false;
 
 //Can define variable, and then initialize later
 var car;
 car = 'toyota';
 
+
+/*** PRINT TO TERMINAL ***/
+
+// console.log prints to the terminal or developer console if running in a browser
+console.log(quantity);
+console.log(price);
+console.log(hello);
+console.log(moreText);
+
+console.log("Easy mode? " + easyMode);
+console.log("Is it summer yet? " + summer);
+
+
+/*** TYPE COERCION ***/
+
 var information = 'coffee';   //Initialize as a string
+console.log(information);
 information = 1234;         // turn into an integer.
+console.log(information);
 //JS is cool with this, but avoid in code, often leads to bugs
+
 
 //Type coercion
 var data = 10;
@@ -22,8 +46,17 @@ console.log('data plus 1 is ' + data + 1);   // data coerced into a string 'Data
 console.log(data + 1 + ' is data plus 1');   // compare to output of this:  '11 is data plus 1'
 
 var string_number = '123';
-var times_two = string_number * 2;    // string_number is coerced to a numerical value
+var times_two = string_number * 2;  // string_number is coerced to a numerical value
 console.log(times_two);            // 246
+
+var pet = "cat";
+var cat_times_two = pet * 2;
+console.log(cat_times_two);     // Can't make sense of this - NaN
+
+
+/*** STRINGS ***/
+
+var text = 'hello world';
 
 var stringLength = text.length; //length is a property
 
@@ -43,32 +76,31 @@ var replaceO = text.replace('o', '0');  //Replace the first instance of first th
 
 var replaceAllO = text.replace(/o/g, '0');  //Replace the all instance of first thing with second thing
 
-
 var removeWhitespace = text.trim();   //Remove spaces, tabs, newlines etc. from both ends of the string
 
 console.log(stringLength, shout, whisper, whereIsW, whereIsZ,
   whisperAndShout, replaceO, replaceAllO, removeWhitespace);
 
 
-//boolean variable
-var inputValid = false;
+/*** ARRAY CREATION, ACCESS ***/
 
 //Array variable (like a list in Python)
+var quiz_scores = [100, 78, 93, 81, 100, 99];
+console.log(quiz_scores);
+
+//Another array. Can mix types if needed
 var my_array = [5, 6, 'cake', 1234.124, text];
-
-//A simple object. More on this later
-var productQuantities = { 'android': 5, 'iphone': 3 };
-
-console.log(quantity);
-console.log(price);
-console.log(text);
-console.log(inputValid);
 
 console.log(my_array);
 console.log(my_array[2]);
 console.log(my_array[-10]);   //non-existent index: this prints 'undefined'
 console.log(my_array[10]);    //non-existent index: this prints 'undefined'
 
+
+/*** OBJECTS ***/
+
+//A simple object. More on this later
+var productQuantities = { 'android': 5, 'iphone': 3 };
 console.log(productQuantities);
 
 //Two ways to access things in an object
@@ -76,64 +108,9 @@ console.log(productQuantities.android);  // Dot notation, if you know the key
 console.log(productQuantities['android']); //or square brackets, if the key is from a variable
 
 
+/*** IF ELSE ***/
 
-//for loop, repeat a certain number of times
-for ( var x = 0 ; x < 10 ; x++) {
-  console.log(x);
-}
-
-var letters = ['a', 'b', 'c'];
-
-//loop over an array
-for (var i = 0 ; i < letters.length ; i++) {
-  console.log(letters[i]);
-}
-
-//Loop over all properties in an object
-var pc_specs = { 'CPU' : '3.0 GHz', 'Memory' : '4GB'};
-
-for (var spec in pc_specs) {
-  console.log(spec, pc_specs[spec]);
-}
-
-//The loop above works for a simple object. With more complex objects
-//you may have inherited properties that you may wish to exclude. So modify loop as follows
-for (var spec in pc_specs) {
-  if (pc_specs.hasOwnProperty(spec)) { //check that spec is an actual property of this object
-    console.log(spec, pc_specs[spec]);
-  }
-}
-
-
-/* A while loop to double a number and display it
-Stop when a number greater than 100 is reached */
-
-var maxVal = 100;
-var number = 1;
-while (number < maxVal) {
-  number *= 2;
-  console.log('number = ' + number)
-}
-
-/* Another way of looping over an array. A non-existent array
-element is 'undefined' and JavaScript considers 'undefined' = false.
-Anything that has a value is considered to be true (except the value false)
-What would happen if your array had 'false' or something that evaluated to undefined in it?
- */
-var animals = ['Buffalo', 'Elephant', 'Yak'];
-var a = 0;
-while (animals[a]){
-  console.log(animals[a]);
-  a++;
-}
-
-//Infinite loop!
-//Avoid. Scripts that take too long to run or appear to be stuck
-//in infinite loops, will be killed by the browser.
-
-//while (true) {
-//  console.log ('hello');
-//}
+// Note that == and != work for comparisons, but it's recommended to use === and !== instead
 
 var grade = 100;
 
@@ -148,8 +125,6 @@ if (orderQuantity < 0) {
   // note that you need to escape special characters. Or use different quotes
   // console.log("Error - can't order a negative number");
 }
-
-
 
 
 /* if else statements */
@@ -175,7 +150,6 @@ if (cities.indexOf('Minneapolis') === -1) {
 }
 
 console.log(cities);
-
 
 
 //&& represents logical AND
@@ -238,33 +212,127 @@ else {
 }
 
 
+// Why === and == ?
+
+var number_5 = 5;
+var string_5 = '5';
+
+// == double equals operator
+console.log(number_5 == string_5);  // True
+// Types are coerced and the string '5' is considered == equal to the number 5
+
+// === triple equals operator
+console.log(number_5 === string_5);  // False
+// False. Types are NOT coerced, the two operands must be the same type,
+// as well as the same value
+// The string '5' is NOT considered === equal to the number 5
+
+
+// != not equals operator
+console.log(number_5 != string_5);  // False
+/* Types are coerced and the string '5' is considered == equal to the number 5
+ So checking non-equality between '5' and 5 return false - they are not, not equal */
+
+// !== not equals operator
+console.log(number_5 !== string_5);  // True
+/* False. Types are NOT coerced, the two operands must be the same type,
+ as well as the same value
+ The string '5' is NOT considered === equal to the number 5 */
+
+
+/*** LOOPS ***/
+
+//for loop, repeat a certain number of times
+for ( var x = 0 ; x < 10 ; x++) {
+  console.log(x);
+}
+
+
+/* A while loop to double a number and display it
+Stop when a number greater than 100 is reached */
+var maxVal = 100;
+var number = 1;
+while (number < maxVal) {
+  number *= 2;
+  console.log('number = ' + number)
+}
+
+
+//Infinite loop!
+//Avoid. Scripts that take too long to run or appear to be stuck
+//in infinite loops, will be killed by the browser.
+
+//while (true) {
+//  console.log ('hello');
+//}
+
+
+/*** LOOPS WITH ARRAYS ***/
+
+var letters = ['a', 'b', 'c'];
+
+//loop over an array by counting the elements
+for (var i = 0 ; i < letters.length ; i++) {
+  console.log('Element ' + i + ' is ' + letters[i]);
+}
+
+
+/* Another way of looping over an array.
+More concise if you don't need to know the position of the elements
+Notice the function definition in the function call! */
+var animals = ['Buffalo', 'Elephant', 'Yak'];
+animals.forEach(function(element){
+  console.log(element);
+});
+
+
+/*** LOOPS WITH OBJECTS ***/
+
+//Loop over all properties in an object
+var pc_specs = { 'CPU' : '3.0 GHz', 'Memory' : '4GB'};
+
+for (var spec in pc_specs) {
+  console.log(spec, pc_specs[spec]);
+}
+
+//The loop above works for a simple object. With more complex objects
+//you may have inherited properties that you may wish to exclude. So modify loop as follows
+for (var spec in pc_specs) {
+  if (pc_specs.hasOwnProperty(spec)) { //check that spec is an actual property of this object
+    console.log(spec, pc_specs[spec]);
+  }
+}
+
+
+/*** ARRAY MANIPULATION ***/
+
 birds = ['Parrot', 'Chicken', 'Dodo', 'Owl'];
 
-birds[1] = 'Eagle';   //replace element 1 with 'Eagle'
-birds[5] = 'Sparrow';  //JS lets you do this...
-                  // Empty elements are added to fill in the gaps.
+birds[1] = 'Eagle';   // replace element 1 with 'Eagle'
+birds[5] = 'Sparrow';  // JS lets you do this. There is no element 5, but 'Sparrow'
+                    // is added at position 5, and undefined elements are added to fill in the gaps.
 console.log(birds);
 
-birds[4] = 'Jay';   //Fil in the blank element
+birds[4] = 'Jay';   //Fill in the blank element
 console.log(birds);
 
-var firstBird = birds[0];    //Get a copy of data from the array
+var firstBird = birds[0];    // Read data a copy of data from the array
 var thirdBird = birds[2];
 console.log(firstBird, thirdBird);
 
-lastBird = birds.pop();         //Removes last item and returns it
+var lastBird = birds.pop();         // Removes last item and returns it
 console.log(lastBird, birds);
-birds.push('Seagull');          //Push adds new element to end of array
+birds.push('Seagull');          // Push adds new element to end of array
 console.log(birds);
 
-firstBird = birds.shift();      //Remove and returns first element
+firstBird = birds.shift();      // Remove and returns first element
 console.log(firstBird, birds);
-birds.unshift('Wren');          //Add element to start of array
+birds.unshift('Wren');          // Add element to start of array
 console.log(birds);
 
 
 //How many elements in the array?
-numberOfBirds = birds.length;
+var numberOfBirds = birds.length;
 console.log(numberOfBirds);
 
 //Where is an element in the array? Return index if found, -1 if not
@@ -272,7 +340,6 @@ console.log(birds.indexOf('Owl'));    // 3
 console.log(birds.indexOf('Penguin'));    // -1
 
 //Loop over array
-
 for (var j = 0 ; j < birds.length ; j++) {
   console.log('Bird ' + j + ' is ' + birds[j]);
 }
@@ -291,6 +358,8 @@ var allBirdsWithSeparator = birds.join(' % ');  //Join all elements,
 console.log(allBirds);
 console.log(allBirdsWithSeparator);
 
+
+/*** FUNCTIONS ***/
 
 var sum = add(1, 2);
 console.log(sum);
@@ -340,35 +409,4 @@ do_something(1);
 do_something(1, 2);
 do_something(1, 2, 3);
 do_something(1, 2, 3, 4);
-
-
-var number_5 = 5;
-var string_5 = '5';
-
-// == double equals operator
-console.log(number_5 == string_5);  // True
-// Types are coerced and the string '5' is considered == equal to the number 5
-
-// === triple equals operator
-console.log(number_5 === string_5);  // False
-// False. Types are NOT coerced, the two operands must be the same type,
-// as well as the same value
-// The string '5' is NOT considered === equal to the number 5
-
-
-
-
-// != not equals operator
-console.log(number_5 != string_5);  // False
-/* Types are coerced and the string '5' is considered == equal to the number 5
- So checking non-equality between '5' and 5 return false - they are not, not equal */
-
-// !== not equals operator
-console.log(number_5 !== string_5);  // True
-/* False. Types are NOT coerced, the two operands must be the same type,
- as well as the same value
- The string '5' is NOT considered === equal to the number 5 */
-
-
-
 
