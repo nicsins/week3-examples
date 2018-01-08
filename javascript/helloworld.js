232b11733b2c9320ccfcb438142602bb69e5cc95
clara <clara@clara.com>
2018-01-08 11:28:53 -0600

    Tidy slice and splice

--- javascript/helloworld.js
+++ javascript/helloworld.js
@@ -5,8 +5,8 @@ var quantity = 6;
 var price = 123.45;
 
 //String variables. Use single or double quotes
-var text = "hello world";
-var moreText = 'single quotes are ok too';
+var text = 'hello world';
+var moreText = "double quotes are ok too";
 
 //Can define variable, and then initialize later
 var car;
@@ -31,17 +31,17 @@ var shout = text.toUpperCase();  //These are methods - convert to uppercase
 
 var whisper = text.toLowerCase();   //And to lowercase
 
-var whereIsW = text.indexOf("w");   //Index of first matching character or -1 if not found
+var whereIsW = text.indexOf('w');   //Index of first matching character or -1 if not found
 
-var whereIsZ = text.indexOf("z");   //Index of first matching character or -1 if not found
+var whereIsZ = text.indexOf('z');   //Index of first matching character or -1 if not found
 
 var whisperAndShout = whisper.concat(shout);  //Join strings together
 
-var replaceO = text.replace("o", "0");  //Replace the first instance of first thing with second thing
+var replaceO = text.replace('o', '0');  //Replace the first instance of first thing with second thing
                                         //See reference for more info on this, can also do global
                                         //and more complex replacements with regular expressions
 
-var replaceAllO = text.replace(/o/g, "0");  //Replace the all instance of first thing with second thing
+var replaceAllO = text.replace(/o/g, '0');  //Replace the all instance of first thing with second thing
 
 
 var removeWhitespace = text.trim();   //Remove spaces, tabs, newlines etc. from both ends of the string
@@ -54,10 +54,10 @@ console.log(stringLength, shout, whisper, whereIsW, whereIsZ,
 var inputValid = false;
 
 //Array variable (like a list in Python)
-var my_array = [5, 6, "cake", 1234.124, text];
+var my_array = [5, 6, 'cake', 1234.124, text];
 
 //A simple object. More on this later
-var productQuantities = { "android": 5, "iphone": 3 };
+var productQuantities = { 'android': 5, 'iphone': 3 };
 
 console.log(quantity);
 console.log(price);
@@ -66,8 +66,8 @@ console.log(inputValid);
 
 console.log(my_array);
 console.log(my_array[2]);
-console.log(my_array[-10]);   //non-existent index: this prints "undefined"
-console.log(my_array[10]);    //non-existent index: this prints "undefined"
+console.log(my_array[-10]);   //non-existent index: this prints 'undefined'
+console.log(my_array[10]);    //non-existent index: this prints 'undefined'
 
 console.log(productQuantities);
 
@@ -90,7 +90,7 @@ for (var i = 0 ; i < letters.length ; i++) {
 }
 
 //Loop over all properties in an object
-var pc_specs = { "CPU" : "3.0 GHz", "Memory" : "4GB"};
+var pc_specs = { 'CPU' : '3.0 GHz', 'Memory' : '4GB'};
 
 for (var spec in pc_specs) {
   console.log(spec, pc_specs[spec]);
@@ -112,15 +112,15 @@ var maxVal = 100;
 var number = 1;
 while (number < maxVal) {
   number *= 2;
-  console.log("number = " + number)
+  console.log('number = ' + number)
 }
 
 /* Another way of looping over an array. A non-existent array
-element is "undefined" and JavaScript considers "undefined" = false.
+element is 'undefined' and JavaScript considers 'undefined' = false.
 Anything that has a value is considered to be true (except the value false)
 What would happen if your array had 'false' or something that evaluated to undefined in it?
  */
-var animals = ["Buffalo", "Elephant", "Yak"];
+var animals = ['Buffalo', 'Elephant', 'Yak'];
 var a = 0;
 while (animals[a]){
   console.log(animals[a]);
@@ -132,44 +132,46 @@ while (animals[a]){
 //in infinite loops, will be killed by the browser.
 
 //while (true) {
-//  console.log ("hello");
+//  console.log ('hello');
 //}
 
 var grade = 100;
 
 if (grade === 100) {
-  console.log("You aced the quiz!");
+  console.log('You aced the quiz!');
 }
 
 var orderQuantity = -1;
 
 if (orderQuantity < 0) {
-  console.log("Error - can't order a negative number");
+  console.log('Error - can\'t order a negative number');
+  // note that you need to escape special characters. Or use different quotes
+  // console.log("Error - can't order a negative number");
 }
 
 
 
 
 /* if else statements */
-var animal = "zebra";
+var animal = 'zebra';
 
-if (animal === "zebra") {
-  console.log("This animal is a zebra");
+if (animal === 'zebra') {
+  console.log('This animal is a zebra');
 } else {
-  console.log("Not a zebra");
+  console.log('Not a zebra');
 }
 
 
 
-var cities = ["London", "Paris", "Berlin"];
+var cities = ['London', 'Paris', 'Berlin'];
 
 // array.indexOf(var) returns -1 if element not found
-//If "Minneapolis" not in our array, then add (push)
+//If 'Minneapolis' not in our array, then add (push)
 //it to the end of the array
-if (cities.indexOf("Minneapolis") === -1) {
-  cities.push("Minneapolis");
+if (cities.indexOf('Minneapolis') === -1) {
+  cities.push('Minneapolis');
 } else {
-  console.log("Minneapolis is in the array of cities")
+  console.log('Minneapolis is in the array of cities')
 }
 
 console.log(cities);
@@ -181,7 +183,7 @@ console.log(cities);
 
 var itemPrice = 100;
 if (itemPrice === 100 && orderQuantity > 1) {
-  console.log("You have ordered at 1 or more $100 items");
+  console.log('You have ordered at 1 or more $100 items');
 }
 
 /* || represents logical OR
@@ -191,59 +193,59 @@ var newCustomer = true;
 var haveEmail = false;
 
 if (newCustomer === true || haveEmail === false) {
-  console.log("Need to request customer's email")
+  console.log('Need to request customer\'s email');
 }
 
 //More concise version. ! reverses the truth of a value.
 
 if (newCustomer || !haveEmail) {
-  console.log("Need to request customer's email")
+  console.log('Need to request customer\'s email');
 }
 
 
 
-console.log("Based on the number of legs a creature has, I will try to guess what it is.");
+console.log('Based on the number of legs a creature has, I will try to guess what it is.');
 
 var legs = 4;    /* Pretend this is user input */
 
 if (legs < 0) {
-  console.log("I don't think anything has a negative number of legs");
+  console.log('I don\'t think anything has a negative number of legs');
 }
 else if (legs % 2 !== 0) {
-  console.log("An odd number of legs, are you sure?");
+  console.log('An odd number of legs, are you sure?');
 }
 else if (legs === 0) {
-  console.log("Is it a snake?");
+  console.log('Is it a snake?');
 }
 else if (legs === 2) {
-  console.log("A biped animal, such as a human?");
+  console.log('A biped animal, such as a human?');
 }
 else if (legs === 4) {
-  console.log("A quadruped, like a zebra?");
+  console.log('A quadruped, like a zebra?');
 }
 else if (legs === 6) {
-  console.log("Probably an insect, like a bee?");
+  console.log('Probably an insect, like a bee?');
 }
 else if (legs === 8) {
-  console.log("Possibly a spider?");
+  console.log('Possibly a spider?');
 }
 else if (legs > 8) {
-  console.log("More than 8 legs - perhaps a millipede?");
+  console.log('More than 8 legs - perhaps a millipede?');
 }
 else {
-  console.log("This shouldn't happen, " +
-    "but add an else clause anyway to help detect if it does");
+  console.log('This shouldn\'t happen, ' +
+    'but add an else clause anyway to help detect if it does');
 }
 
 
-birds = ["Parrot", "Chicken", "Dodo", "Owl"];
+birds = ['Parrot', 'Chicken', 'Dodo', 'Owl'];
 
-birds[1] = "Eagle";   //replace element 1 with "Eagle"
-birds[5] = "Sparrow";  //JS lets you do this...
+birds[1] = 'Eagle';   //replace element 1 with 'Eagle'
+birds[5] = 'Sparrow';  //JS lets you do this...
                   // Empty elements are added to fill in the gaps.
 console.log(birds);
 
-birds[4] = "Jay";   //Fil in the blank element
+birds[4] = 'Jay';   //Fil in the blank element
 console.log(birds);
 
 var firstBird = birds[0];    //Get a copy of data from the array
@@ -252,12 +254,12 @@ console.log(firstBird, thirdBird);
 
 lastBird = birds.pop();         //Removes last item and returns it
 console.log(lastBird, birds);
-birds.push("Seagull");          //Push adds new element to end of array
+birds.push('Seagull');          //Push adds new element to end of array
 console.log(birds);
 
 firstBird = birds.shift();      //Remove and returns first element
 console.log(firstBird, birds);
-birds.unshift("Wren");          //Add element to start of array
+birds.unshift('Wren');          //Add element to start of array
 console.log(birds);
 
 
@@ -266,13 +268,13 @@ numberOfBirds = birds.length;
 console.log(numberOfBirds);
 
 //Where is an element in the array? Return index if found, -1 if not
-console.log(birds.indexOf("Owl"));    // 3
-console.log(birds.indexOf("Penguin"));    // -1
+console.log(birds.indexOf('Owl'));    // 3
+console.log(birds.indexOf('Penguin'));    // -1
 
 //Loop over array
 
 for (var j = 0 ; j < birds.length ; j++) {
-  console.log("Bird " + j + " is " + birds[j]);
+  console.log('Bird ' + j + ' is ' + birds[j]);
 }
 
 birds.reverse();  //Reverse order of elements
@@ -284,7 +286,7 @@ console.log(birds);
 
 var allBirds = birds.join();  //Join all elements into a string,
                                         // separated by commas
-var allBirdsWithSeparator = birds.join(" % ");  //Join all elements,
+var allBirdsWithSeparator = birds.join(' % ');  //Join all elements,
                                               // connected with a custom string
 console.log(allBirds);
 console.log(allBirdsWithSeparator);
@@ -298,17 +300,17 @@ function add(a, b) {
 }
 
 
-exampleArray = ["Plankton", "Squid", "Sea Sponge"];
-exampleArray2 = ["Owl", "Puffin", "Eagle"];
+exampleArray = ['Plankton', 'Squid', 'Sea Sponge'];
+exampleArray2 = ['Owl', 'Puffin', 'Eagle'];
 
 console.log(containsOwl(exampleArray));
 console.log(containsOwl(exampleArray2));
 
-/* A very specific function testing if an array contains "Owl"
+/* A very specific function testing if an array contains 'Owl'
 in any case. How could you make this more generally useful? */
 function containsOwl(array) {
 
-  var owl = "owl";
+  var owl = 'owl';
 
   for (var i = 0; i < array.length ; i++) {
     if (array[i].toLowerCase() === owl) {
@@ -341,7 +343,7 @@ do_something(1, 2, 3, 4);
 
 
 var number_5 = 5;
-var string_5 = "5";
+var string_5 = '5';
 
 // == double equals operator
 console.log(number_5 == string_5);  // True
